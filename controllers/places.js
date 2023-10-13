583cbb8e7f29693bc70b2082d13188bde5cf7509
Christian Alquisada <calquisada@aol.com>
2023-10-13 07:41:06 -0700

    commit

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,22 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+   res.render('GET /places')
 })
 
+// GET /places
+app.get('/', (req, res) => {
+   res.render('places/index')
+ })
+ 
+
 module.exports = router
+
+
+/*  router.get('/', (req, res) => {
+    res.render('GET /places')
+ }) */
+
+ /* router.get('/', (req, res) => {
+    res.send('GET /places')
+ }) */
---
+++ views/places/index.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('./default')
+
+function index () {
+    return (
+        <Def>
+            <main>
+                <h1>Index</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = index
