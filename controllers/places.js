ad56ec4cafc3aedbbb1fc7356f6882cc6276f37a
Christian Alquisada <calquisada@aol.com>
2023-10-17 19:08:51 -0700

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -67,7 +67,25 @@ router.get('/:id/edit', (req, res) => {
   }
 })
 
-
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    if (!req.body.pic) {
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.state = 'USA'
+    }
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
 
 
  module.exports = router
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,22 +1,36 @@
 const React = require('react')
 const Def = require('../default.jsx')
 
-function edit_form (data) {
+function edit_form (data) { 
     return (
         <Def>
             <main>
                 <h1>Edit Place</h1>
                 <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
-                    <div className="form-group">
-                        <label htmlFor="name">Place Name</label>
-                        <input 
-                        className="form-control" 
-                        id="name" 
-                        name="name" 
-                        value={data.place.name} 
-                        required />
+                    <div className="row">
+                            <div className="form-group col-sm-6">
+                                    <label htmlFor="name">Place Name</label>
+                                    <input id="name" name="name"/>
+                            </div>
+                            <div className="form-group col-sm-6">
+                                <label htmlFor="pic">Place Picture</label>
+                                <input id="pic" name="pic" />
+                            </div>
+                            <div className="form-group col-sm-6">
+                                <label htmlFor="City">City</label>
+                                <input id="city" name="city" />
+                            </div>
+                            <div className="form-group col-sm-6">
+                                <label htmlFor="state">State</label>
+                                <input id="state" name="state" />
+                            </div>
+                            <div className="form-group col-sm-6">
+                                <label htmlFor="cuisines">Cuisines</label>
+                                <input id="cuisines" name="cuisines" />
+                            </div>
+                            <input className="btn btn-primary" type="submit" value="Add Place" />
                     </div>
-                </form>
+                    </form>
             </main>
         </Def>
     )
