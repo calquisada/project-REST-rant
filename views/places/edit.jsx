54b97d2b3c2c6bef206362fedb8f45867f7e68f6
Christian Alquisada <calquisada@aol.com>
2023-10-18 17:24:44 -0700

    "Added the edit functionality"

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,14 +6,14 @@ function edit_form (data) {
         <Def>
             <main>
                 <h1>Edit Place</h1>
-                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
                 <div className="form-group">
                         <label htmlFor="name">Place Name</label>
                         <input className="form-control" id="name" name="name" value={data.place.name} required />
                     </div>
                     <div className="form-group">
                             <label htmlFor="pic">Place Picture</label>
-                            <input className="form-control" type="url" id="pic" name="pic" value={data.place.pic} />
+                            <input className="form-control" id="pic" name="pic" value={data.place.pic} />
                     </div>
                     <div className="form-group">
                             <label htmlFor="city">City</label>
