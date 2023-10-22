0e9569b354d1c70eae2b876479813d85319c9115
Christian Alquisada <calquisada@aol.com>
2023-10-21 17:23:17 -0700

    commit

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,109 @@
 const router = require('express').Router()
+const db = require('../models')
+
+router.get('/', (req, res) => {
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err) 
+      res.render('error404')
+    })
+})
+
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  res.send('GET /places/:id stub')
+})
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const router = require('express').Router()
 const places = require('../models/places.js')
 
 router.get('/new', (req, res) => {
@@ -90,9 +195,8 @@ router.put('/:id', (req, res) => {
   }
 })
 
-
-
  module.exports = router
+ */
  
 /*  router.get('/', (req, res) => {
     res.render('GET /places')
--- models/places.js
+++ models/places.js
@@ -1,13 +1,40 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/weera-thai1-1024x683.jpg'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/1200px-Internet_cafe_golden_princess.jpg'
-}]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//module.exports = [{
+  //  name: 'H-Thai-ML',
+   // city: 'Seattle',
+   // state: 'WA',
+   // cuisines: 'Thai, Pan-Asian',
+   // pic: '/images/weera-thai1-1024x683.jpg'
+//}, {
+  //  name: 'Coding Cat Cafe',
+  //  city: 'Phoenix',
+   // state: 'AZ',
+   // cuisines: 'Coffee, Bakery',
+   // pic: '/images/1200px-Internet_cafe_golden_princess.jpg'
+//}]
