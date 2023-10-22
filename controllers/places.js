9bf30fed234fed17855e66f078b477b6535c43d3
Christian Alquisada <calquisada@aol.com>
2023-10-21 17:27:59 -0700

    commit

--- controllers/places.js
+++ controllers/places.js
@@ -29,8 +29,16 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-  res.send('GET /places/:id stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
+
 
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
