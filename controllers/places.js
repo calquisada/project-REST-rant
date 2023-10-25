c1eb249cf409453c617f55b6f6355b45c6fe49d9
Christian Alquisada <calquisada@aol.com>
2023-10-24 20:06:01 -0700

    commit

--- controllers/places.js
+++ controllers/places.js
@@ -24,6 +24,7 @@ router.post('/', (req, res) => {
 })
 
 
+
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -29,7 +29,7 @@ function new_form() {
                     </div>
                     <div className="form-group">
                             <label for="founded">Founded Year</label>
-                            <input className="form-control" id="founded" name="founded" />
+                            <input type="number" className="form-control" id="founded" name="founded" value={new Date().getFullYear()} />
                     </div>
 
                             <input className="btn btn-primary" type="submit" value="Add Place" />
