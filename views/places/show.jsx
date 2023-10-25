d0ceed09e53c7616eef910002dca38855b68617b
Christian Alquisada <calquisada@aol.com>
2023-10-24 19:42:33 -0700

    commit

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,10 +6,13 @@ function show (data) {
         <Def>
           <main>
             <h1>{data.place.name}</h1>
+            <img src={data.place.pic} alt={data.place.name} />
             <h2>Rating</h2>
               <p>Not Rated</p>
             <h2>Comments</h2> 
               <p>No comments yet!</p> 
+            <h3>Located in {data.place.city}, {data.place.state}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
             </a>  
