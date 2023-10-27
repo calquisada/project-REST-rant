f7acd2d96b332d19d5f655c777af101604552734
Christian Alquisada <calquisada@aol.com>
2023-10-27 01:00:12 -0700

    commit

--- controllers/places.js
+++ controllers/places.js
@@ -29,9 +29,12 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+// Show
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -39,6 +42,7 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
+
 
 
 router.put('/:id', (req, res) => {
@@ -53,9 +57,27 @@ router.get('/:id/edit', (req, res) => {
   res.send('GET edit form stub')
 })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
+
 
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,7 +2,26 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-    return (
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
+  return (
         <Def>
           <main>
             <h1>{data.place.name}</h1>
@@ -10,7 +29,7 @@ function show (data) {
             <h2>Rating</h2>
               <p>Not Rated</p>
             <h2>Comments</h2> 
-              <p>No comments yet!</p> 
+              {comments}
             <h3>Located in {data.place.city}, {data.place.state}</h3>
             <h4>Serving {data.place.cuisines}</h4>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
@@ -20,10 +39,40 @@ function show (data) {
             <button type="submit" className="btn btn-danger">
             Delete
             </button>
-            </form>     
+            </form>
+            <h2>Comments</h2>
+            <div className="row">
+              {comments}
+            </div>
+            <hr />
+            <h2>Got Your Own Rant or Rave?</h2>
+            <form action={`/places/${data.place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="stars">Star Rating</label>
+                  <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="form-group col-sm-2">
+                  <label htmlFor="rant">Rant?</label>
+                  <input type="checkbox" id="rant" name="rant" className="form-control" />
+                </div>
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
+            </form>   
           </main>
         </Def>
     )
 }
 
 module.exports = show
+
